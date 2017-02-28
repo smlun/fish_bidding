cc632d48de2371cc2767bd27994b756232b54433
Ming Lun <smlun@msn.com>
2017-02-28 15:48:53 +0800

    Set index page

--- fishbidding/config/routes.rb
+++ fishbidding/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :sale_posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #set sale_posts#index as homepage
+  root 'sale_posts#index'
 end
